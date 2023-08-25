b010562211e3d6b2e66155e5c2874303f672279b
salva <juliansalvatierra@gmail.com>
2023-08-25 12:42:40 -0300

    se agrego el aside del product detail y se soluciono conflictos

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,18 +15,18 @@ console.log("js funciona")
 
 
 function toggleDesktopMenu() {
-    const isCarritoAsideClosed = aside.classList.contains('inactive');
+    const isCarritoAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isCarritoAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isCarritoAsideClosed = aside.classList.contains('inactive');
+    const isCarritoAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if(!isCarritoAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -39,7 +39,7 @@ function toggleCarritoAside() {
     if(!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
